cb6c8acea04544b9ed807fa0afe8b174ace37612
Santiago Garcia <santiagogarcia97@outlook.com>
2022-09-02 10:47:07 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,7 @@ const App = () => {
           </tbody>
         </table>
         <div className="absolute right-0 top-0 text-xs">
-          Actualizado: 01/09/2022 13:40
+          Actualizado: 02/09/2022 10:45
         </div>
       </div>
 
